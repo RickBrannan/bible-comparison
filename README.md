03cf08018f54bde39cb5b5702e62cb6385d89143
Rick Brannan <43759793+RickBrannan@users.noreply.github.com>
2023-10-25 18:27:51 -0700

    Initial release bible-comparison

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # bible-comparison
-Code to compare bible versions and determine word differences
+
+Python code that uses [a customized version](https://github.com/google/diff-match-patch/wiki/Line-or-Word-Diffs) of [Google's `diff-match-patch`](https://github.com/google/diff-match-patch) to compare bible versions and determine word differences
+
+Google's diff-match-patch is used under the Apache License 2.0. 
+
+## License
+
+This work is licensed under the MIT 
+
+* Google's diff-match-patch is used under the Apache License 2.0. The copy in this repo has been modified according to [these instructions](https://github.com/google/diff-match-patch/wiki/Line-or-Word-Diffs).
+* SBLGNT is available under a CC-BY 4.0 license from [Logos Bible Software](https://github.com/LogosBible/SBLGNT).
+* Nestle 1904 is available from [Clear Bible (Biblica)](https://github.com/Clear-Bible/macula-greek/Nestle1904).
+
+# input format
+
+The input format is a simple tab-delimited file that uses an encoded verse number (supported by biblelib) and Greek text.
+
+```
+Verse	Words
+40001001	Βίβλος γενέσεως Ἰησοῦ χριστοῦ υἱοῦ Δαυὶδ υἱοῦ Ἀβραάμ.
+40001002	Ἀβραὰμ ἐγέννησεν τὸν Ἰσαάκ, Ἰσαὰκ δὲ ἐγέννησεν τὸν Ἰακώβ, Ἰακὼβ δὲ ἐγέννησεν τὸν Ἰούδαν καὶ τοὺς ἀδελφοὺς αὐτοῦ,
+40001003	Ἰούδας δὲ ἐγέννησεν τὸν Φαρὲς καὶ τὸν Ζάρα ἐκ τῆς Θαμάρ, Φαρὲς δὲ ἐγέννησεν τὸν Ἑσρώμ, Ἑσρὼμ δὲ ἐγέννησεν τὸν Ἀράμ,
+```
+
+The `data/tsv/` folder provides two examples:
+
+* Nestle 1904, sourced from [Clear-Bible's Macula Greek project](https://github.com/Clear-Bible/macula-greek) and in the public domain
+* SBLGNT, sourced from [Clear-Bible's Macula Greek project](https://github.com/Clear-Bible/macula-greek) and [CC-BY licensed by Logos Bible Software](https://github.com/LogosBible/SBLGNT)
+
+# output format
+
+For this we're simply dumping the output from diff-match-patch to STDOUT. It looks like this:
+
+```
+Current: REV 22:5 (66022005)
+[(0, 'και νυξ οὐκ ἐσται ἐτι και οὐκ ἐχουσιν χρειαν φωτος λυχνου και '), (-1, 'φωτος '), (1, 'φως '), (0, 'ἡλιου ὁτι κυριος ὁ θεος φωτισει ἐπ αὐτους και βασιλευσουσιν εἰς τους αἰωνας των αἰωνων')]
+Current: REV 22:6 (66022006)
+Exact match for 66022006
+Current: REV 22:7 (66022007)
+Exact match for 66022007
+Current: REV 22:8 (66022008)
+[(0, 'κἀγω '), (-1, 'ἰωανης '), (1, 'ἰωαννης '), (0, 'ὁ ἀκουων και βλεπων ταυτα και ὁτε ἠκουσα και ἐβλεψα ἐπεσα προσκυνησαι ἐμπροσθεν των ποδων του ἀγγελου του δεικνυοντος μοι ταυτα')]
+```
+
+In this view:
+
+* `0` indicates words in common between the `source` and `compare` editions.
+* `-1` indicates words in the `source` edition but not in the `compare` edition.
+* `1` indicates words in the `compare` edition but not in the `source` edition.
+
+Note that the compared texts have been normalized for case (lower-case) and accents have been removed.
--- code/python/bible-comparison/compare_verses.py
+++ code/python/bible-comparison/compare_verses.py
@@ -12,13 +12,13 @@ import diff_match_patch as dmp_module
 @dataclasses.dataclass
 class Word:
     identifier: str
-    alt_id: str
+    # alt_id: str
     text: str
-    strongs: str
-    gloss: str
-    gloss2: str
-    pos: str
-    morph: str
+    # strongs: str
+    # gloss: str
+    # gloss2: str
+    # pos: str
+    # morph: str
 
 @dataclasses.dataclass
 class Verse:
@@ -31,21 +31,22 @@ class Verse:
     words: dict[str, Word] = dataclasses.field(default_factory=list)
 
 
-def load_bsbgnt_lines():
-    print("Loading BSBGNT")
+def load_lines(edition, edition_file_name):
+    print(f"Loading {edition}")
     return_lines = {}
-    previous_verse_id = "41001001"
+    previous_verse_id = "40001001"
     current_verse_id = ""
     words = {}
-    with open('C:/git/Clear/internal-Alignments/data/bsb/BGNT-BSB_source.tsv', 'r', encoding='utf-8') as infile:
+    with open(f'{git_dir}data/tsv/{edition_file_name}', 'r', encoding='utf-8') as infile:
         lines = infile.readlines()
         for line in lines:
-            if line.startswith('identifier'):
+            if line.startswith('Verse'):
                 continue
             line = line.rstrip('\n')
             # split the line on tabs and stitch things together
             cols = line.split('\t')
-            bcv = BCVWPID(cols[0])
+            # bcv = BCVWPID(cols[0])
+            bcv = BCVID(cols[0])
             # create the verse object if it doesn't exist
             current_verse_id = bcv.book_ID + bcv.chapter_ID + bcv.verse_ID
             if previous_verse_id != current_verse_id:
@@ -57,49 +58,23 @@ def load_bsbgnt_lines():
                 words = {}
 
             # create the word object
-            word = Word(cols[0], cols[1], cols[2], cols[3], cols[4], cols[5], cols[6], cols[7])
-            # remove non-word chars from word.text
-            word.text = re.sub(r'\W+', '', word.text)
-            # word.text = word.text.replace('*', '').replace('|', '')
-            words[word.identifier] = word
+            # split on space
+            verse_words = cols[1].split(' ')
+            word_in_verse = 1
+            for current_word in verse_words:
+                # zeropad word_in_verse with leading zeros
+                word_id = current_verse_id + str(word_in_verse).zfill(3)
+                # remove non-word chars from word.text. option to ignore case, remove accents, etc.?
+                word = Word(word_id, current_word)
+                word.text = re.sub(r'\W+', '', word.text)
+                words[word.identifier] = word
+                word_in_verse += 1
 
         bcv = BCVID(current_verse_id)
         return_lines[current_verse_id] = Verse(current_verse_id, bcv.book_ID, bcv.chapter_ID, bcv.verse_ID, bcv.to_usfm(), words)
 
     return return_lines
 
-def load_sblgnt_lines():
-    print("Loading SBLGNT")
-    return_lines = {}
-    previous_verse_id = "41001001"
-    current_verse_id = ""
-    words = {}
-    sblgnt_tsv = 'C:/git/Clear/macula-greek/SBLGNT/tsv/macula-greek-SBLGNT.tsv'
-    df = pd.read_csv(sblgnt_tsv, sep='\t', header=0, dtype=str, encoding='utf-8', keep_default_na=False)
-    for row in df.index:
-        # remove initial 'n' from identifier
-        identifier = df['xml:id'][row][1:]
-        bcv = BCVWPID(identifier)
-        # create the verse object if it doesn't exist
-        current_verse_id = bcv.book_ID + bcv.chapter_ID + bcv.verse_ID
-        if previous_verse_id != current_verse_id:
-            # we need to dump verse info.
-            previous_bcv = BCVID(previous_verse_id)
-            return_lines[previous_verse_id] = Verse(previous_verse_id, previous_bcv.book_ID, previous_bcv.chapter_ID,
-                                                    previous_bcv.verse_ID, previous_bcv.to_usfm(), words)
-            previous_verse_id = current_verse_id
-            words = {}
-        # create the word object
-        # id, alt-id, text, strongs, gloss, gloss2, pos, morph
-        word = Word(identifier, "", df['text'][row], df['strong'][row], df['gloss'][row], df['english'][row],
-                    df['class'][row], df['morph'][row])
-        # remove non-word chars from word.text
-        word.text = re.sub(r'\W+', '', word.text)
-        words[word.identifier] = word
-
-    bcv = BCVID(current_verse_id)
-    return_lines[current_verse_id] = Verse(current_verse_id, bcv.book_ID, bcv.chapter_ID, bcv.verse_ID, bcv.to_usfm(), words)
-    return return_lines
 
 def get_verse_text(verse):
     return_text = ""
@@ -117,153 +92,59 @@ def diff_wordMode(text1, text2):
     lineArray = initial_diff[2]
     diffs = dmp.diff_main(wordText1, wordText2, False)
     dmp.diff_charsToLines(diffs, lineArray)
-    # does this cause problems with splitting variations within words? (e.g. Mt 14:27; 15:30)?
+    # diff_cleanupSemantic() causes issues when comparing word-level
     # dmp.diff_cleanupSemantic(diffs)
     return diffs
 
 
-def get_words_and_ids(gnt, gnt_verse, gnt_count):
-    return_words_and_ids = {}
-    for word in gnt:
-        gnt_word = list(gnt_verse.words.items())[gnt_count][1]
-        # regex to remove non-word chars from gnt_word.text
-        gnt_word_text = re.sub(r'\W+', '', gnt_word.text)
-        return_words_and_ids[gnt_word_text] = gnt_word.identifier
-        gnt_count += 1
-    return return_words_and_ids
+# some globals
+git_dir = "C:/git/RickBrannan/bible-comparison/"
 
+# need to load editions in a lines format
+# both source_gnt_lines and compare_gnt_lines are dict[Verse.identifier, Verse] so verses can be sorted properly
+source_edition = "n1904"
+compare_edition = "sblgnt"
+source_gnt_lines = load_lines(source_edition, "N1904-verses.tsv")
+compare_gnt_lines = load_lines(compare_edition, "sblgnt-verses.tsv")
 
-def map_variant_verse(diff, bsbgnt_verse, sblgnt_verse, map_bsbgnt_to_sblgnt):
-    bsbgnt_count = 0
-    sblgnt_count = 0
-    bsbgnt_ids = list(bsbgnt_verse.words.keys())
-    sblgnt_ids = list(sblgnt_verse.words.keys())
-
-    diff_item_count = 0
-    bool_skip = False
-
-    for item in diff:
-        if item[0] == 0:
-            # same for both verses
-            for word in item[1].strip().split(' '):
-                map_bsbgnt_to_sblgnt[bsbgnt_ids[bsbgnt_count]] = sblgnt_ids[sblgnt_count]
-                bsbgnt_count += 1
-                sblgnt_count += 1
-            bool_skip = False
-        # need to determine if -1 and 1 are a paired instance, or it is marking presence vs. absence
-        elif (item[0] == -1) and (len(diff) == (diff_item_count + 1)):
-            # this is a bsb addition at end of verse. there is nothing to map it to in sblgnt.
-            bsb = item[1].strip().split(' ')
-            for word in bsb:
-                # map to nothing in SBLGNT
-                map_bsbgnt_to_sblgnt[bsbgnt_ids[bsbgnt_count]] = ""
-                bsbgnt_count += 1
-            sblgnt_count += 1
-            bool_skip == False
-        elif (item[0] == -1 and diff[diff_item_count + 1][0] == 1) and bool_skip == False:
-            # paired?
-            # trim whitespace from item[1]
-            bsb = item[1].strip().split(' ')
-            sbl = diff[diff_item_count + 1][1].strip().split(' ')
-
-            if (len(bsb) == 1) and (len(sbl) == 1):
-                map_bsbgnt_to_sblgnt[bsbgnt_ids[bsbgnt_count]] = sblgnt_ids[sblgnt_count]
-                bsbgnt_count += 1
-                sblgnt_count += 1
-            elif (len(bsb) == 1) and (len(sbl) > 1):
-                # one-to-many mapping
-                sbl_ids = []
-                for word in sbl:
-                    sbl_ids.append(sblgnt_ids[sblgnt_count])
-                    sblgnt_count += 1
-                map_bsbgnt_to_sblgnt[bsbgnt_ids[bsbgnt_count]] = sbl_ids
-                bsbgnt_count += 1
-            elif (len(sbl) == 1) and (len(bsb) > 1):
-                # many-to-one mapping. Unsure how to accomplish that in present model.
-                for word in bsb:
-                    map_bsbgnt_to_sblgnt[bsbgnt_ids[bsbgnt_count]] = sblgnt_ids[sblgnt_count]
-                    bsbgnt_count += 1
-                sblgnt_count += 1
-            elif len(sbl) == len(bsb):
-                # if the length is equivalent, then we should be able to sort the words
-                # alphabetically (dict[word.text, word.identifier] and map most (all?)
-                # of them correctly based on word matching
-                bsb_words_and_ids = get_words_and_ids(bsb, bsbgnt_verse, bsbgnt_count)
-                sbl_words_and_ids = get_words_and_ids(sbl, sblgnt_verse, sblgnt_count)
-                # sort the words
-                bsb_words = list(bsb_words_and_ids.keys())
-                bsb_words.sort()
-                sbl_words = list(sbl_words_and_ids.keys())
-                sbl_words.sort()
-                # map the words
-                var_word_count = 0
-                for word in bsb_words:
-                    sbl_var_word = list(sbl_words_and_ids.items())[var_word_count][0]
-                    map_bsbgnt_to_sblgnt[bsb_words_and_ids[word]] = sbl_words_and_ids[sbl_var_word]
-                    var_word_count += 1
-                    bsbgnt_count += 1
-                    sblgnt_count += 1
-            # elif counts are both greater than one but unequal. likely some word-matching gymnastics
-            else:
-                print("Huh?")
-            bool_skip = True
-        else:
-            # huh?
-            bool_skip = False
-        diff_item_count += 1
-
-    return map_bsbgnt_to_sblgnt
-
-
-
-
-# need to load in SBLGNT in a lines format
-# both bsbgnt_lines and sblgnt_lines are dict[Verse.identifier, Verse] so verses can be sorted properly
-# bsbgnt loads from the 'source' text alignment tab-delimited file
-bsbgnt_lines = load_bsbgnt_lines()
-# sblgnt loads from the Macula Greek SBLGNT tsv file
-sblgnt_lines = load_sblgnt_lines()
-
-# get keys from bsbgnt_lines and sort
+# get keys from source_gnt_lines and sort
 # because I don't know if dicts in python preserve order
-bsbgnt_keys = list(bsbgnt_lines.keys())
-bsbgnt_keys.sort()
+source_gnt_keys = list(source_gnt_lines.keys())
+source_gnt_keys.sort()
 
+# some counters
 verse_match_count = 0
 verse_difference_count = 0
-# this is a word-level map
-map_bsbgnt_to_sblgnt = {}
 
-
-for bsb_verse in bsbgnt_keys:
-    bcv = BCVID(bsb_verse)
+for source_gnt_verse in source_gnt_keys:
+    bcv = BCVID(source_gnt_verse)
+    print(f"Current: {bcv.to_usfm()} ({source_gnt_verse})")
     # we have the same verse in SBLGNT?
-    if sblgnt_lines.__contains__(bsb_verse):
-        bsbgnt_verse_text = get_verse_text(bsbgnt_lines[bsb_verse])
-        sblgnt_verse_text = get_verse_text(sblgnt_lines[bsb_verse])
+    if compare_gnt_lines.__contains__(source_gnt_verse):
+        source_gnt_verse_text = get_verse_text(source_gnt_lines[source_gnt_verse])
+        compare_gnt_verse_text = get_verse_text(compare_gnt_lines[source_gnt_verse])
 
         # if the verses are exact, we skip everything and log it
-        if bsbgnt_verse_text == sblgnt_verse_text:
-            print("Exact match for " + bsb_verse)
+        if source_gnt_verse_text == compare_gnt_verse_text:
+            print("Exact match for " + source_gnt_verse)
             verse_match_count += 1
-            # update word map
-            for word in bsbgnt_lines[bsb_verse].words:
-                # same verse, same number of words, equality between verses, means ids are the same.
-                map_bsbgnt_to_sblgnt[word] = word
             continue
 
         # do the diff
-        diff = diff_wordMode(bsbgnt_verse_text, sblgnt_verse_text)
+        #  0 == text that matches between editions
+        # -1 == text that is in source_edition but not compare_edition
+        #  1 == text that is in compare_edition but not source_edition
+        diff = diff_wordMode(source_gnt_verse_text, compare_gnt_verse_text)
         verse_difference_count += 1
-        print(f"Current: {bcv.to_usfm()} ({bsb_verse})")
         print(diff)
-        map_bsbgnt_to_sblgnt = map_variant_verse(diff, bsbgnt_lines[bsb_verse], sblgnt_lines[bsb_verse],
-                                                 map_bsbgnt_to_sblgnt)
-        # print(diff)
     else:
-        print(f"BSB {bcv.to_usfm()} ({bsb_verse}) not in SBLGNT")
+        print(f"{source_edition} {bcv.to_usfm()} ({source_gnt_verse}) not in {compare_edition}")
 
-# we also need to find the SBLGNT verses that are not in BSBGNT (e.g. 3Jn 1:15)
+# we also need to find the compare_edition verses that are not in source_edition (e.g. 3Jn 1:15)
+for compare_verse in compare_gnt_lines:
+    if not source_gnt_lines.__contains__(compare_verse):
+        bcv = BCVID(compare_verse)
+        print(f"{compare_edition} {bcv.to_usfm()} ({compare_edition}) not in {source_edition}")
 
 print(f"Verse match count: {verse_match_count}")
 print(f"Verse difference count: {verse_difference_count}")
